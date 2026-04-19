ba3f68fe7b1e23c84ede039d78be115684968259
Gazala939 <Gazalajamadar786@gmail.com>
2026-04-19 20:02:27 +0530

    with async and await

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-function login(){
+async function login(){
     let username =document.getElementById("userName").value;
     let password =document.getElementById("password").value;
 
-    let message = document.getElementById("message")
+    let message = document.getElementById("message");
 
     message.innerText = "Checking........"
 
     //promises
-    let loginPromise = new Promise(function(resolve,reject){
+    function loginPromise(){
+        return new Promise(function(resolve,reject){
 
         setTimeout(function(){
 
@@ -20,15 +21,18 @@ function login(){
                 reject("Invalid Username or Password")
             }
         },2000)
+        
     });
+}
 
-    loginPromise
-    .then(function(result){
-        message.innerText = result
+    try{
+        let result = await loginPromise()
+        message.innerText = result;
         console.log(result)
-    })
-    .catch(function(error){
-        message.innerText = error
+    }catch(error){
+        message.innerText = error;
         console.log(error)
-    })
+    }
+    
+    
 }
